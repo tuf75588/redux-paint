2417c4ce5eadc289309a33a5e09db98d62c1fa88
Andrew Davis <atd285@gmail.com>
2021-03-25 13:49:50 -0400

    fix package typo

--- src/lib/canvasUtils.ts
+++ src/lib/canvasUtils.ts
@@ -29,3 +29,12 @@ export const getCanvasImage = (
     canvas.toBlob(resolve);
   });
 };
+
+export const clearCanvas = (canvas: HTMLCanvasElement) => {
+  const context = canvas.getContext('2d');
+  if (!context) {
+    return;
+  }
+  context.fillStyle = 'white';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+};
